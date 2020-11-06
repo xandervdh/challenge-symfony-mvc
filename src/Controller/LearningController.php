25b8063e5c61991b92e2735c6e96eb573261a1c3
xandervdh <xander-v.d.h@hotmail.com>
2020-11-06 12:44:51 +0100

    made some new pages and trying to make the session work

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -5,9 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class LearningController extends AbstractController
 {
+    private string $name = "unknown";
+    private Session $session;
+
+    /**
+     * LearningController constructor.
+     */
+    public function __construct()
+    {
+        $this->session = new Session();
+    }
+
+
     /**
      * @Route("/learning", name="learning")
      */
@@ -19,21 +32,36 @@ class LearningController extends AbstractController
     }
 
     /**
-     * @Route("/about-me", name="about me")
+     * @Route("/about-me", name="aboutMe")
      */
     public function aboutMe(): Response
     {
-       return $this->render('learning/aboutMe.html.twig', ['name' => 'BeCode']);
+       return $this->render('learning/aboutMe.html.twig', [
+           'name' => $this->name,
+       ]);
     }
 
-    public function showMyName()
+    /**
+     * @Route("/", name="showname")
+     */
+    public function showMyName() :response
     {
-        
+        $this->session->start();
+        if (isset($_SESSION['name'])){
+            $this->name = $_SESSION['name'];
+        } else {
+            $this->name = 'Unknown';
+        }
+        return $this->render('learning/showName.html.twig', ['name' => $this->name]);
     }
 
+    /**
+     * @Route("/change-name", name="changename",  methods={"POST"})
+     */
     public function changeMyName()
     {
-        
+        $_SESSION['name'] = $_POST['name'];
+        return $this->redirectToRoute('showname');
     }
 
 }
